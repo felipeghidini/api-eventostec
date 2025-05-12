04afaeca772dc61b358c2e18e402f82a9dd22c0f
felipeghidini <lipeghidini18@gmail.com>
2025-05-12 06:25:37 -0300

    feat: projeto iniciado e incluido classes de event e address

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,19 @@
+package com.eventostec.api.domain.address;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+
+}
