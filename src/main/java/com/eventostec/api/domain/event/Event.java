80344d1092f2e12c9100abba630506a68641f207
felipeghidini <lipeghidini18@gmail.com>
2025-07-28 08:26:09 -0300

    feat: add CouponRequestDTO

--- src/main/java/com/eventostec/api/domain/event/Event.java
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -14,8 +14,8 @@ import java.util.UUID;
 
 @Table(name = "event")
 @Entity
-@Setter
-@Getter
+//@Setter
+//@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
@@ -29,4 +29,60 @@ public class Event {
     private String eventUrl;
     private Boolean remote;
     private Date date;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    public Boolean getRemote() {
+        return remote;
+    }
+
+    public void setRemote(Boolean remote) {
+        this.remote = remote;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
